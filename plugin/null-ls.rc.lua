657bea2d25ebb860c3d99122bda087280206e2d5
Kocheng <devil871018@gmail.com>
2022-09-08 13:02:42 +0800

    fix auto formatting

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -1,22 +1,20 @@
-local null_ls = require("null-ls")
+local status, null_ls = pcall(require, "null-ls")
 
-local formatting = null_ls.builtins.formatting
-
-local sources = {
-	-- formatting.eslint,
-	-- formatting.prettier,
-	-- formatting.latexindent,
-	-- formatting.autopep8,
-	-- formatting.stylua,
-	-- formatting.clang_format,
-}
+if(not status)then
+  return
+end
 
 null_ls.setup({
-	sources = sources,
-
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			-- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
-		end
-	end,
+  sources = {
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.pretterd,
+  },
+  on_attach = function(client, _)
+    if client.server_capabilities.documentFormattingProVider then
+      vim.api.nvim_command [[augroup Format]]
+      vim.api.nvim_command [[autocmd! * <buffer>]]
+      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+      vim.api.nvim_command [[augroup END]]
+    end
+  end,
 })
