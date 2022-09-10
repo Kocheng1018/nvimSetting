e9fc93c6fcd1586aeaf5362403f9035926c1afd0
Kocheng <devil871018@gmail.com>
2022-09-10 13:27:32 +0800

    change ident blcnk tab char

--- after/plugin/indent-blankline.rc.lua
+++ after/plugin/indent-blankline.rc.lua
@@ -1,13 +1,9 @@
 vim.opt.list = true
 
-vim.opt.listchars:append(
-  {tab = ">-"},
-  {eol = "↴"}
-)
-
 vim.cmd [[highlight IndentBlankline guifg=#5C6370 gui=nocombine]]
 
 require("indent_blankline").setup {
+    char = "⎸",
     space_char_blankline = " ",
     show_end_of_line = true,
     show_current_context = false,
